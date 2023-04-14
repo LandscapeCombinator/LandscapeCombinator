d000267a404cb10a706e2e50e3625d6a3d8510f5
LandscapeCombinator <landscapecombinator@proton.me>
2023-04-14 12:28:23 +0200

    Add synchronous download functions

--- Source/LandscapeCombinator/Private/Utils/Download.cpp
+++ Source/LandscapeCombinator/Private/Utils/Download.cpp
@@ -10,6 +10,110 @@
 
 namespace Download {
 
+	float SleepSeconds = 0.05;
+	float TimeoutSeconds = 10;
+
+	bool SynchronousFromURL(FString URL, FString File)
+	{
+		UE_LOG(LogLandscapeCombinator, Log, TEXT("Downloading '%s' to '%s'"), *URL, *File);
+
+		if (ExpectedSizeCache.Contains(URL))
+		{
+			UE_LOG(LogLandscapeCombinator, Log, TEXT("Cache says expected size for '%s' is '%d'"), *URL, ExpectedSizeCache[URL]);
+			return SynchronousFromURLExpecting(URL, File, ExpectedSizeCache[URL]);
+		}
+		else
+		{
+			TSharedRef<IHttpRequest> Request = FHttpModule::Get().CreateRequest();
+			Request->SetURL(URL);
+			Request->SetVerb("HEAD");
+			Request->SetHeader("User-Agent", "X-UnrealEngine-Agent");
+
+			bool bIsComplete = false;
+			int32 ExpectedSize = 0;
+
+			Request->OnProcessRequestComplete().BindLambda([URL, File, &ExpectedSize, &bIsComplete](FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful) {
+				if (bWasSuccessful && Response.IsValid() && EHttpResponseCodes::IsOk(Response->GetResponseCode()))
+				{
+					ExpectedSize = FCString::Atoi(*Response->GetHeader("Content-Length"));
+				}
+				bIsComplete = true;
+			});
+			Request->ProcessRequest();
+
+			float StartTime = FPlatformTime::Seconds();
+			while (!bIsComplete && FPlatformTime::Seconds() - StartTime <= TimeoutSeconds)
+			{
+				FPlatformProcess::Sleep(SleepSeconds);
+			}
+
+			Request->CancelRequest();
+
+			return SynchronousFromURLExpecting(URL, File, ExpectedSize);
+		}
+	}
+
+	bool SynchronousFromURLExpecting(FString URL, FString File, int32 ExpectedSize)
+	{
+		IPlatformFile &PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
+		if (PlatformFile.FileExists(*File) && ExpectedSize != 0 && PlatformFile.FileSize(*File) == ExpectedSize)
+		{
+			UE_LOG(LogLandscapeCombinator, Log, TEXT("File already exists with the correct size, skipping download of '%s' to '%s' "), *URL, *File);
+			return true;
+		}
+
+		bool bDownloadResult = false;
+		bool bIsComplete = false;
+
+		TSharedRef<IHttpRequest> Request = FHttpModule::Get().CreateRequest();
+		Request->SetURL(URL);
+		Request->SetVerb("GET");
+		Request->SetHeader("User-Agent", "X-UnrealEngine-Agent");
+		Request->OnProcessRequestComplete().BindLambda([URL, File, &bDownloadResult, &bIsComplete](FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful)
+		{
+			IPlatformFile &PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
+			bool DownloadSuccess = bWasSuccessful && Response.IsValid() && EHttpResponseCodes::IsOk(Response->GetResponseCode());
+			bool SavedFile = false;
+			if (DownloadSuccess)
+			{
+				SavedFile = FFileHelper::SaveArrayToFile(Response->GetContent(), *File);
+				if (SavedFile)
+				{
+					AddExpectedSize(URL, PlatformFile.FileSize(*File));
+					bDownloadResult = true;
+					bIsComplete = true;
+
+					UE_LOG(LogLandscapeCombinator, Log, TEXT("Finished downloading '%s' to '%s'"), *URL, *File);
+				}
+				else
+				{
+					bDownloadResult = false;
+					bIsComplete = true;
+					UE_LOG(LogLandscapeCombinator, Error, TEXT("Error while saving '%s' to '%s'"), *URL, *File);
+				}
+			}
+			else
+			{
+				UE_LOG(LogLandscapeCombinator, Error, TEXT("Error while downloading '%s' to '%s'"), *URL, *File);
+				UE_LOG(LogLandscapeCombinator, Error, TEXT("Request was not successful. Error %d."), Response->GetResponseCode());
+				bDownloadResult = false;
+				bIsComplete = true;
+			}
+		});
+		Request->ProcessRequest();
+		
+
+		float StartTime = FPlatformTime::Seconds();
+		while (!bIsComplete && FPlatformTime::Seconds() - StartTime <= TimeoutSeconds)
+		{
+			FPlatformProcess::Sleep(SleepSeconds);
+		}
+		
+		Request->CancelRequest();
+
+		return bDownloadResult;
+	}
+
 	void FromURL(FString URL, FString File, TFunction<void(bool)> OnComplete)
 	{
 		UE_LOG(LogLandscapeCombinator, Log, TEXT("Downloading '%s' to '%s'"), *URL, *File);
--- Source/LandscapeCombinator/Public/Utils/Download.h
+++ Source/LandscapeCombinator/Public/Utils/Download.h
@@ -6,8 +6,14 @@
 
 namespace Download {
 	TMap<FString, int> ExpectedSizeCache; 
+	
+	static bool SynchronousFromURLExpecting(FString URL, FString File, int32 ExpectedSize);
+	static bool SynchronousFromURL(FString URL, FString File);
+
 	static void FromURLExpecting(FString URL, FString File, int32 ExpectedSize, TFunction<void(bool)> OnComplete);
 	static void FromURL(FString URL, FString File, TFunction<void(bool)> OnComplete);
+
+
 	void AddExpectedSize(FString URL, int32 ExpectedSize);
 	FString ExpectedSizeCacheFile();
 	void LoadExpectedSizeCache();
