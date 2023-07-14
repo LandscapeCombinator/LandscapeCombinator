beeaa517b05f114ecda5bda114ca431de55a94af
LandscapeCombinator <landscapecombinator@proton.me>
2023-07-14 08:53:27 +0200

    Fix some bugs in the Download API

--- Source/LandscapeCombinator/Private/Utils/Download.cpp
+++ Source/LandscapeCombinator/Private/Utils/Download.cpp
@@ -3,6 +3,7 @@
 #include "Utils/Download.h"
 #include "Utils/Logging.h"
 #include "LandscapeCombinatorStyle.h"
+#include "Async/Async.h"
 
 #include "Http.h"
 #include "Misc/FileHelper.h"
@@ -14,6 +15,9 @@ namespace Download {
 	float SleepSeconds = 0.05;
 	float TimeoutSeconds = 10;
 }
+
+// FIXME: use TAtomic<bool> for bTriggered, but with TAtomic<bool> bTriggered { false}, everything blocks
+// We use bTriggered to avoid OnProcessRequestComplete being invoked multiple times
 
 bool Download::SynchronousFromURL(FString URL, FString File)
 {
@@ -33,8 +37,11 @@ bool Download::SynchronousFromURL(FString URL, FString File)
 
 		bool bIsComplete = false;
 		int32 ExpectedSize = 0;
-
-		Request->OnProcessRequestComplete().BindLambda([URL, File, &ExpectedSize, &bIsComplete](FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful) {
+		
+		bool *bTriggered = new bool(false);
+		Request->OnProcessRequestComplete().BindLambda([URL, File, &ExpectedSize, &bIsComplete, &bTriggered](FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful) {
+			if (*bTriggered) return;
+			*bTriggered = true;
 			if (bWasSuccessful && Response.IsValid() && EHttpResponseCodes::IsOk(Response->GetResponseCode()))
 			{
 				ExpectedSize = FCString::Atoi(*Response->GetHeader("Content-Length"));
@@ -71,8 +78,11 @@ bool Download::SynchronousFromURLExpecting(FString URL, FString File, int32 Expe
 	Request->SetURL(URL);
 	Request->SetVerb("GET");
 	Request->SetHeader("User-Agent", "X-UnrealEngine-Agent");
-	Request->OnProcessRequestComplete().BindLambda([URL, File, &bDownloadResult, &bIsComplete](FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful)
+	bool *bTriggered = new bool(false);
+	Request->OnProcessRequestComplete().BindLambda([URL, File, &bDownloadResult, &bIsComplete, &bTriggered](FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful)
 	{
+		if (*bTriggered) return;
+		*bTriggered = true;
 		IPlatformFile &PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
 		bool DownloadSuccess = bWasSuccessful && Response.IsValid() && EHttpResponseCodes::IsOk(Response->GetResponseCode());
 		bool SavedFile = false;
@@ -118,7 +128,7 @@ bool Download::SynchronousFromURLExpecting(FString URL, FString File, int32 Expe
 
 void Download::FromURL(FString URL, FString File, TFunction<void(bool)> OnComplete)
 {
-	UE_LOG(LogLandscapeCombinator, Log, TEXT("Downloading '%s' to '%s'"), *URL, *File);
+	UE_LOG(LogLandscapeCombinator, Log, TEXT("Downloading from URL '%s' to '%s'"), *URL, *File);
 
 	if (ExpectedSizeCache.Contains(URL))
 	{
@@ -131,7 +141,11 @@ void Download::FromURL(FString URL, FString File, TFunction<void(bool)> OnComple
 		Request->SetURL(URL);
 		Request->SetVerb("HEAD");
 		Request->SetHeader("User-Agent", "X-UnrealEngine-Agent");
-		Request->OnProcessRequestComplete().BindLambda([URL, File, OnComplete](FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful) {
+		bool *bTriggered = new bool(false);
+		Request->OnProcessRequestComplete().BindLambda([URL, File, OnComplete, &bTriggered](FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful) {
+			if (*bTriggered) return;
+			*bTriggered = true;
+
 			if (bWasSuccessful && Response.IsValid() && EHttpResponseCodes::IsOk(Response->GetResponseCode()))
 			{
 				int32 ExpectedSize = FCString::Atoi(*Response->GetHeader("Content-Length"));
@@ -149,107 +163,114 @@ void Download::FromURL(FString URL, FString File, TFunction<void(bool)> OnComple
 
 void Download::FromURLExpecting(FString URL, FString File, int32 ExpectedSize, TFunction<void(bool)> OnComplete)
 {
-	double *Downloaded = new double();
-
-
-	TSharedPtr<SWindow> Window = SNew(SWindow)
-		.SizingRule(ESizingRule::Autosized)
-		.AutoCenter(EAutoCenter::PrimaryWorkArea)
-		.Title(LOCTEXT("DownloadProgress", "Download Progress"));
-
-	IPlatformFile &PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
-	if (PlatformFile.FileExists(*File) && ExpectedSize != 0 && PlatformFile.FileSize(*File) == ExpectedSize)
+	// make sure we are in game thread to spawn download progress windows
+	AsyncTask(ENamedThreads::GameThread, [=]()
 	{
-		UE_LOG(LogLandscapeCombinator, Log, TEXT("File already exists with the correct size, skipping download of '%s' to '%s' "), *URL, *File);
-		if (OnComplete) OnComplete(true);
-		return;
-	}
+		double *Downloaded = new double();
 
-	TSharedPtr<IHttpRequest> Request = FHttpModule::Get().CreateRequest().ToSharedPtr();
-	Request->SetURL(URL);
-	Request->SetVerb("GET");
-	Request->SetHeader("User-Agent", "X-UnrealEngine-Agent");
-	Request->OnRequestProgress().BindLambda([Downloaded](FHttpRequestPtr Request, int32 Sent, int32 Received) {
-		*Downloaded = Received;
-	});
-	Request->OnProcessRequestComplete().BindLambda([URL, File, OnComplete, Window](FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful)
-	{
-		Window->RequestDestroyWindow();
+		TSharedPtr<SWindow> Window = SNew(SWindow)
+			.SizingRule(ESizingRule::Autosized)
+			.AutoCenter(EAutoCenter::PrimaryWorkArea)
+			.Title(LOCTEXT("DownloadProgress", "Download Progress"));
+
 		IPlatformFile &PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
-		bool DownloadSuccess = bWasSuccessful && Response.IsValid() && EHttpResponseCodes::IsOk(Response->GetResponseCode());
-		bool SavedFile = false;
-		if (DownloadSuccess)
+		if (PlatformFile.FileExists(*File) && ExpectedSize != 0 && PlatformFile.FileSize(*File) == ExpectedSize)
 		{
-			SavedFile = FFileHelper::SaveArrayToFile(Response->GetContent(), *File);
-			if (SavedFile)
+			UE_LOG(LogLandscapeCombinator, Log, TEXT("File already exists with the correct size, skipping download of '%s' to '%s' "), *URL, *File);
+			if (OnComplete) OnComplete(true);
+			return;
+		}
+
+		TSharedPtr<IHttpRequest> Request = FHttpModule::Get().CreateRequest().ToSharedPtr();
+		Request->SetURL(URL);
+		Request->SetVerb("GET");
+		Request->SetHeader("User-Agent", "X-UnrealEngine-Agent");
+		Request->OnRequestProgress().BindLambda([Downloaded](FHttpRequestPtr Request, int32 Sent, int32 Received) {
+			*Downloaded = Received;
+		});
+		bool *bTriggered = new bool(false);
+		Request->OnProcessRequestComplete().BindLambda([URL, File, OnComplete, Window, bTriggered](FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful)
+		{
+			if (*bTriggered) return;
+			*bTriggered = true;
+
+			IPlatformFile &PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
+			bool DownloadSuccess = bWasSuccessful && Response.IsValid() && EHttpResponseCodes::IsOk(Response->GetResponseCode());
+			bool SavedFile = false;
+			if (DownloadSuccess)
 			{
-				AddExpectedSize(URL, PlatformFile.FileSize(*File));
-				UE_LOG(LogLandscapeCombinator, Log, TEXT("Finished downloading '%s' to '%s'"), *URL, *File);
+				SavedFile = FFileHelper::SaveArrayToFile(Response->GetContent(), *File);
+				if (SavedFile)
+				{
+					AddExpectedSize(URL, PlatformFile.FileSize(*File));
+					UE_LOG(LogLandscapeCombinator, Log, TEXT("Finished downloading '%s' to '%s'"), *URL, *File);
+				}
+				else
+				{
+					UE_LOG(LogLandscapeCombinator, Error, TEXT("Error while saving '%s' to '%s'"), *URL, *File);
+				}
 			}
 			else
 			{
-				UE_LOG(LogLandscapeCombinator, Error, TEXT("Error while saving '%s' to '%s'"), *URL, *File);
+				UE_LOG(LogLandscapeCombinator, Error, TEXT("Error while downloading '%s' to '%s'"), *URL, *File);
+				if (Response.IsValid())
+				{
+					UE_LOG(LogLandscapeCombinator, Error, TEXT("Request was not successful. Error %d."), Response->GetResponseCode());
+				}
 			}
-		}
-		else
-		{
-			UE_LOG(LogLandscapeCombinator, Error, TEXT("Error while downloading '%s' to '%s'"), *URL, *File);
-			if (Response.IsValid())
-			{
-				UE_LOG(LogLandscapeCombinator, Error, TEXT("Request was not successful. Error %d."), Response->GetResponseCode());
-			}
-		}
-		if (OnComplete) OnComplete(DownloadSuccess && SavedFile);
-	});
+			if (OnComplete) OnComplete(DownloadSuccess && SavedFile);
+			Window->RequestDestroyWindow();
+		});
 
 
-	Request->ProcessRequest();
+		Request->ProcessRequest();
 
-	Window->SetContent(
-		SNew(SBox).Padding(FMargin(30, 30, 30, 30))
-		[
-			SNew(SVerticalBox)
-			+SVerticalBox::Slot().AutoHeight()
+		Window->SetContent(
+			SNew(SBox).Padding(FMargin(30, 30, 30, 30))
 			[
-				SNew(STextBlock).Text(
-					FText::Format(
-						LOCTEXT("DowloadingURL", "Dowloading {0} to {1}."),
-						FText::FromString(URL.Left(20)),
-						FText::FromString(File)
-					)
-				).Font(FLandscapeCombinatorStyle::RegularFont())
-			]
-			+SVerticalBox::Slot().AutoHeight().Padding(FMargin(0, 0, 0, 20))
-			[
-				SNew(SProgressBar)
-				.Percent_Lambda([Downloaded, ExpectedSize, Request]() {
-					if (ExpectedSize) return *Downloaded / ExpectedSize;
-					return *Downloaded / MAX_int32;
-				})
-				.RefreshRate(0.1)
-			]
-			+SVerticalBox::Slot().AutoHeight().HAlign(EHorizontalAlignment::HAlign_Center)
-			[
-				SNew(SHorizontalBox)
-				+SHorizontalBox::Slot().AutoWidth().HAlign(EHorizontalAlignment::HAlign_Center)
+				SNew(SVerticalBox)
+				+SVerticalBox::Slot().AutoHeight()
 				[
-					SNew(SButton)
-					.OnClicked_Lambda([Window, Request]()->FReply {
-						Request->CancelRequest();
-						Window->RequestDestroyWindow();
-						return FReply::Handled();
+					SNew(STextBlock).Text(
+						FText::Format(
+							LOCTEXT("DowloadingURL", "Dowloading {0} to {1}."),
+							FText::FromString(URL.Left(20)),
+							FText::FromString(File)
+						)
+					).Font(FLandscapeCombinatorStyle::RegularFont())
+				]
+				+SVerticalBox::Slot().AutoHeight().Padding(FMargin(0, 0, 0, 20))
+				[
+					SNew(SProgressBar)
+					.Percent_Lambda([Downloaded, ExpectedSize, Request]() {
+						if (ExpectedSize) return *Downloaded / ExpectedSize;
+						return *Downloaded / MAX_int32;
 					})
+					.RefreshRate(0.1)
+				]
+				+SVerticalBox::Slot().AutoHeight().HAlign(EHorizontalAlignment::HAlign_Center)
+				[
+					SNew(SHorizontalBox)
+					+SHorizontalBox::Slot().AutoWidth().HAlign(EHorizontalAlignment::HAlign_Center)
 					[
-						SNew(STextBlock).Font(FLandscapeCombinatorStyle::RegularFont()).Text(FText::FromString(" Cancel "))
+						SNew(SButton)
+						.OnClicked_Lambda([Window, Request]()->FReply {
+							Request->CancelRequest();
+							Window->RequestDestroyWindow();
+							return FReply::Handled();
+						})
+						[
+							SNew(STextBlock).Font(FLandscapeCombinatorStyle::RegularFont()).Text(FText::FromString(" Cancel "))
+						]
 					]
 				]
 			]
-		]
-	);
-	Window->SetOnWindowClosed(FOnWindowClosed::CreateLambda([Request](const TSharedRef<SWindow>& Window) {
-		Request->CancelRequest();
-	}));
-	FSlateApplication::Get().AddWindow(Window.ToSharedRef());
+		);
+		Window->SetOnWindowClosed(FOnWindowClosed::CreateLambda([Request](const TSharedRef<SWindow>& Window) {
+			Request->CancelRequest();
+		}));
+		FSlateApplication::Get().AddWindow(Window.ToSharedRef());
+	});
 }
 
 void Download::AddExpectedSize(FString URL, int32 ExpectedSize)
