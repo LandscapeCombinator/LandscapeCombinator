9f00b9148d1dba27fd9c78cdcba44717ebe05f00
Landscape Combinator <landscapecombinator@proton.me>
2025-04-10 12:37:57 +0200

    Add missing include

--- Source/LCCommon/Public/LCCommon/LCGenerator.h
+++ Source/LCCommon/Public/LCCommon/LCGenerator.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
 #include "UObject/Interface.h"
 #include "Delegates/Delegate.h"
 #include "Kismet/BlueprintAsyncActionBase.h"
